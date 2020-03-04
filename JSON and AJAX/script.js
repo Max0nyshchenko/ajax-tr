bdb77b8ebf775831a92d537cd4e07a026459e973
Max Onyshchenko <max.0nyshenk03@gmail.com>
2020-03-05 00:50:00 +0200

    second commit

---
+++ JSON and AJAX/script.js
@@ -0,0 +1,25 @@
+var thePets  = [
+    {
+        'name': 'Meowsalot',
+        'species': 'cat',
+        'favFood': 'tuna'
+    },
+    {
+        'name': 'Dalot',
+        'species': 'cat',
+        'favFood': 'pasta'
+    },
+    {
+        'name': 'Bawolat',
+        'species': 'dog',
+        'favFood': 'pizza'
+    }
+]
+
+var ourRequest = new XMLHttpRequest();
+ourRequest.open("GET", "https://learnwebcode.github.io/json-example/animals-1.json");
+ourRequest.onload = function() {
+    var ourData = JSON.parse(ourRequest.responseText);
+    console.log(ourData[0]);
+}
+ourRequest.send();
